2a2df549ab9848b8cf3c0f4fac6b315ba53f5778
cho45 <cho45@lowreal.net>
2016-12-11 21:49:54 +0900

    update with graph based

--- mackerel-plugin-h2o/lib/h2o.go
+++ mackerel-plugin-h2o/lib/h2o.go
@@ -14,6 +14,7 @@ import (
 type H2OPlugin struct {
 	URI string
 	prefix      string
+	withDurations bool
 }
 
 func (p H2OPlugin) FetchMetrics() (map[string]interface{}, error) {
@@ -36,56 +37,10 @@ func (p H2OPlugin) parseStats(body io.Reader) (map[string]interface{}, error) {
 		return nil, err
 	}
 
-	common := []string{
-		"status-errors.400",
-		"status-errors.403",
-		"status-errors.404",
-		"status-errors.405",
-		"status-errors.416",
-		"status-errors.417",
-		"status-errors.500",
-		"status-errors.502",
-		"status-errors.503",
-		"http2-errors.protocol",
-		"http2-errors.internal",
-		"http2-errors.flow-control",
-		"http2-errors.settings-timeout",
-		"http2-errors.stream-closed",
-		"http2-errors.frame-size",
-		"http2-errors.refused-stream",
-		"http2-errors.cancel",
-		"http2-errors.compression",
-		"http2-errors.connect",
-		"http2-errors.enhance-your-calm",
-		"http2-errors.inadequate-security",
-		"http2.read-closed",
-		"http2.write-closed",
-	}
-	for _, k := range common {
-		v, ok := s[k].(float64)
+	for k, raw := range s {
+		v, ok := raw.(float64)
 		if ok {
 			stat[k] = v
-		}
-	}
-
-	durations := []string{
-		"connect-time",
-		"header-time",
-		"body-time",
-		"request-total-time",
-		"process-time",
-		"response-time",
-		"duration",
-	}
-	ranges := []string {
-		"0", "25", "50", "75", "99",
-	}
-	for _, k := range durations {
-		for _, r := range ranges {
-			v, ok := s[k + "-" + r].(float64)
-			if ok {
-				stat[k + "." + r] = v
-			}
 		}
 	}
 
@@ -95,7 +50,7 @@ func (p H2OPlugin) parseStats(body io.Reader) (map[string]interface{}, error) {
 func (p H2OPlugin) GraphDefinition() map[string]mp.Graphs {
 	labelPrefix := strings.Title(p.prefix)
 
-	return map[string]mp.Graphs{
+	base := map[string]mp.Graphs{
 		"status-errors": {
 			Label: (labelPrefix + " Status Errors"),
 			Unit: "integer",
@@ -137,84 +92,94 @@ func (p H2OPlugin) GraphDefinition() map[string]mp.Graphs {
 				{Name: "http2.write-closed", Label: "write-closed", Diff: false},
 			},
 		},
+	}
+
+	durations := map[string]mp.Graphs{
 		"connect-time": {
 			Label: (labelPrefix + " Connect Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "connect-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "connect-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "connect-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "connect-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "connect-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "connect-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "connect-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "connect-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "connect-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "connect-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"header-time": {
 			Label: (labelPrefix + " Header Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "header-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "header-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "header-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "header-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "header-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "header-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "header-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "header-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "header-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "header-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"body-time": {
 			Label: (labelPrefix + " Body Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "body-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "body-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "body-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "body-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "body-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "body-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "body-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "body-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "body-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "body-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"request-total-time": {
 			Label: (labelPrefix + " Request Time Total Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "request-total-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "request-total-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "request-total-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "request-total-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "request-total-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "request-total-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "request-total-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "request-total-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "request-total-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "request-total-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"process-time": {
 			Label: (labelPrefix + " Process Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "process-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "process-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "process-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "process-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "process-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "process-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "process-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "process-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "process-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "process-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"response-time": {
 			Label: (labelPrefix + " Response Time"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "response-time.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "response-time.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "response-time.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "response-time.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "response-time.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "response-time-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "response-time-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "response-time-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "response-time-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "response-time-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 		"duration": {
 			Label: (labelPrefix + " Duration"),
 			Unit: "integer",
 			Metrics: []mp.Metrics{
-				{Name: "duration.0", Label: "0", Diff: false, Stacked: true},
-				{Name: "duration.25", Label: "25", Diff: false, Stacked: true},
-				{Name: "duration.50", Label: "50", Diff: false, Stacked: true},
-				{Name: "duration.75", Label: "75", Diff: false, Stacked: true},
-				{Name: "duration.99", Label: "99", Diff: false, Stacked: true},
+				{Name: "duration-0", Label: "0", Diff: false, Stacked: false},
+				{Name: "duration-25", Label: "25", Diff: false, Stacked: false},
+				{Name: "duration-50", Label: "50", Diff: false, Stacked: false},
+				{Name: "duration-75", Label: "75", Diff: false, Stacked: false},
+				{Name: "duration-99", Label: "99", Diff: false, Stacked: false},
 			},
 		},
 	}
+
+	if p.withDurations {
+		for k, v := range durations {
+			base[k] = v
+		}
+	}
+	return base
 }
 
 func (p H2OPlugin) MetricKeyPrefix() string {
@@ -231,6 +196,7 @@ func Do() {
 	optPort := flag.String("port", "80", "Port")
 	optPath := flag.String("path", "/server-status/json", "Path")
 	optBasicAuth := flag.String("basic-auth", "", "BasicAuth")
+	optWithDurations := flag.Bool("with-durations", false, "With durations (requires duration-stats: ON setting on h2o.conf)")
 	optTempfile := flag.String("tempfile", "", "Temp file name")
 	optMetricKeyPrefix := flag.String("metric-key-prefix", "h2o", "Metric Key Prefix")
 	flag.Parse()
@@ -244,6 +210,7 @@ func Do() {
 	}
 
 	h2o.prefix = *optMetricKeyPrefix
+	h2o.withDurations = *optWithDurations
 	helper := mp.NewMackerelPlugin(h2o)
 	helper.Tempfile = *optTempfile
 
